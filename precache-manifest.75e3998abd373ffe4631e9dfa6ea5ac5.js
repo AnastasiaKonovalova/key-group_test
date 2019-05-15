42c3a2c925bc280ce92298ad1843f3b044da5127
AnastasiaKonovalova <silentium901@mail.ru>
2019-05-15 14:45:22 +0400

    Updates

---
+++ precache-manifest.75e3998abd373ffe4631e9dfa6ea5ac5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a1d147f1dfff768722991b84281cb1f8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f8a89cf44b63c07df166",
+    "url": "/static/css/main.797782cf.chunk.css"
+  },
+  {
+    "revision": "9115d353634721b2cf89",
+    "url": "/static/js/2.ce9a5460.chunk.js"
+  },
+  {
+    "revision": "f8a89cf44b63c07df166",
+    "url": "/static/js/main.83b0e412.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
